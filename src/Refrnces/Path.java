e88e6b60ff9fa429dd0eefe3164d0193be2e21f6
MASON <aliasmdev@gmail.com>
2020-07-19 16:03:40 +0430

    done

---
+++ src/Refrnces/Path.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Refrnces;
+
+/**
+ *
+ * @author MASON
+ */
+public class Path 
+{
+    String mainpath="C:\\Users\\MASON\\Desktop\\c.txt";
+    String cupath="C:\\Users\\MASON\\Desktop\\cu.txt";
+
+    public Path() {
+    }
+
+    public String getMainpath() {
+        return mainpath;
+    }
+
+    public String getCupath() {
+        return cupath;
+    }
+    
+    
+}
